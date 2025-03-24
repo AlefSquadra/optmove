0a5f1adc039775d2a80bb142860f41e8ac7671a3
alef.soares <alef.soares@squadra.com.br>
2025-03-24 08:26:02 -0300

    [FEATURE] - Iniciando o repositorio com configurações iniciais

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Login } from '@optmove/design-system'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-white'>
+
+      <Login/>
+      {/* <Input label='Label'/> */}
+    </div>
+    </>
+  )
+}
+
+export default App
