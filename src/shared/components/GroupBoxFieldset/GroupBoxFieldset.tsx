36cf69112ad5bf70ecd095135af8598f7d18e99b
ALEF SANTOS SOARES <CT008311@mrs.com.br>
2025-06-02 19:40:54 +0000

    Merged PR 6710: [FEATURE] - Design front

--- src/shared/components/GroupBoxFieldset/GroupBoxFieldset.tsx
+++
@@ -1,4 +0,0 @@
-import { GroupBoxFieldsetLegend } from "@shared/components/groupBoxFieldset/GroupBoxFieldsetLegend";
-import { GroupBoxFieldsetRoot } from "@shared/components/groupBoxFieldset/GroupBoxFieldsetRoot";
-
-export const GroupBoxFieldset = { Root: GroupBoxFieldsetRoot, Legend: GroupBoxFieldsetLegend };
