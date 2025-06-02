65bad5c6792713624ed035711e5ba9492a988c32
alef.soares <alef.soares@squadra.com.br>
2025-06-02 16:39:35 -0300

    [FIX] - Remove no used fieldset

--- src/shared/components/GroupBoxFieldset/GroupBoxFieldsetLegend.tsx
+++
@@ -1,9 +0,0 @@
-import type { IGroupBoxFieldsetLegendProps } from "@shared/components/groupBoxFieldset/GroupBoxFieldset.type";
-
-export const GroupBoxFieldsetLegend = (props: IGroupBoxFieldsetLegendProps) => {
-  return (
-    <>
-      <legend className="fieldset-legend xl:text-xs">{props?.children}</legend>
-    </>
-  );
-};
