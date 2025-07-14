ccc142955d108019804a9419e4d5b8f1e0902530
alef.soares <alef.soares@squadra.com.br>
2025-07-14 11:34:20 -0300

    [FIX] - Correção para os campos obrigatorios em select

--- src/features/selectZone/types/SelectZoneFormZod.ts
+++ src/features/selectZone/types/SelectZoneFormZod.ts
@@ -3,30 +3,20 @@ import { z } from "zod";
 export const SpecialProfiles = ["VISUALIZADOR", "TESTE", "INTRAJORNADA"] as const;
 
 const optionObjectSchema = z.object({
-  id: z.string().min(1),
+  id: z.string().min(1, "Seleção obrigatória"),
   name: z.string(),
 });
 
-export const SelectZoneFormSchemaZod = z
-  .object({
-    perfilMesa: optionObjectSchema.refine((val) => !!val?.id, {
-      message: "Selecione um perfil para visualização das SBs!",
-    }),
-    mesa: z.array(optionObjectSchema).pipe(z.preprocess((val) => val ?? [], z.array(optionObjectSchema))), // <- solução definitiva
-    zona: optionObjectSchema.refine((val) => !!val?.id, {
-      message: "Selecione uma Zona de Controle!",
-    }),
-  })
-  .superRefine((data, ctx) => {
-    const isPerfilEspecial = SpecialProfiles.includes(data.perfilMesa.id as "VISUALIZADOR" | "TESTE" | "INTRAJORNADA");
-
-    if (!isPerfilEspecial && data.mesa.length === 0) {
-      ctx.addIssue({
-        path: ["mesa"],
-        code: z.ZodIssueCode.custom,
-        message: "Selecione uma mesa de controle!",
-      });
-    }
-  });
+export const SelectZoneFormSchemaZod = z.object({
+  perfilMesa: optionObjectSchema.refine((val) => !!val?.id, {
+    message: "Selecione um perfil para visualização das SBs!",
+  }),
+  mesa: optionObjectSchema.refine((val) => !!val.id, {
+    message: "Selecione uma mesa de controle!",
+  }),
+  zona: optionObjectSchema.refine((val) => !!val?.id, {
+    message: "Selecione uma Zona de Controle!",
+  }),
+});
 
 export type SelectZoneFormValuesZod = z.infer<typeof SelectZoneFormSchemaZod>;
