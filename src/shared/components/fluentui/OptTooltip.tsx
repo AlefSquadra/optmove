349131e502fdeba29368554d7c88c3d7440f05fe
alef.soares <alef.soares@squadra.com.br>
2025-07-14 15:36:18 -0300

    [REFAC] - Simplify OptTooltip component by removing forwardRef

--- src/shared/components/fluentui/OptTooltip.tsx
+++ src/shared/components/fluentui/OptTooltip.tsx
@@ -1,10 +1,7 @@
 import { Tooltip, type TooltipProps } from "@fluentui/react-components";
-import { forwardRef } from "react";
 
-const OptTooltip = forwardRef<HTMLDivElement, TooltipProps>((props, ref) => {
-  return <Tooltip {...props} ref={ref} />;
-});
-
-OptTooltip.displayName = "OptTooltip";
+const OptTooltip = (props: TooltipProps) => {
+  return <Tooltip {...props} />;
+};
 
 export { OptTooltip };
